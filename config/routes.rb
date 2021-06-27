21c539c5c8896650b1e23aa4b9f8d79b329588dc
filipdespic <despicpb12@gmail.com>
2021-06-27 16:50:04 +0200

    add books and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  #if we didnt use resources:users
+  get "/filip", to:"users#show", id:1
+
+  resources :users
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
